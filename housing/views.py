363e557f9fd4167d9a8ae1cf80527db954731651
micahgoodman <micah.s.goodman@gmail.com>
2018-03-27 19:36:50 -0500

    more zillow api setup

--- housing/views.py
+++ housing/views.py
@@ -1,23 +1,32 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+import zillow
+import json
 
-from .models import Choice, Question
-# ...
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+def getInfo(request, property_id):
+    with open("./bin/config/zillow_key.conf", 'r') as f:
+        key = f.readline().replace("\n", "")
+
+    api = zillow.ValuationApi()
+
+    property = get_object_or_404(Property, pk=property_id)
+    selected_property = property.property_list.get(pk=request.POST['address'])
+
+    address = selected_property + ', St. Louis, MO'
+
+    data = api.GetDeepSearchResults(key, address, postal_code)
+
+    return HttpResponse(json.dumps({'foo': 'bar'}), mimetype='application/json')
+
+    #return HttpResponseRedirect(reverse('property', args=(property.id,)))
+
+
+
+
+"""<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+<script type="text/javascript">
+ $.get('/ajax/', function(data) {
+   alert(data['foo']);
+ });
+</script>"""
