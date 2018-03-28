f54e5d243676168eaabdc24bf27d04e70c2660af
Nehemiah I. Dacres <vivacarlie@gmail.com>
2018-03-28 02:16:31 +0000

    replaced database  with sqlite

--- housing/settings.py
+++ housing/settings.py
@@ -77,7 +77,12 @@ WSGI_APPLICATION = 'housing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'housing',
+    }
+}
 
 
 # Internationalization
