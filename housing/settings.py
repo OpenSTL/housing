bab02e1cefe91993ece6125f67947679bf649de3
Nehemiah I. Dacres <vivacarlie@gmail.com>
2018-03-31 00:13:49 +0000

    changed db backing to spacialite

--- housing/settings.py
+++ housing/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'housing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
         'NAME': 'housing',
     }
 }
