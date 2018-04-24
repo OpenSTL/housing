67fd119fa711c5082cbca4c97e73360489e1c024
Nehemiah I. Dacres <vivacarlie@gmail.com>
2018-04-24 05:16:07 +0000

    removed bounding box query added index and byId query

--- housing/views.py
+++ housing/views.py
@@ -14,36 +14,23 @@ def google_forward(request):
     return JsonResponse(r.json())
 
 def lra(request):
-    #top=49.5138 left=10.9351 bottom=49.3866 right=11.201
-    xmin = request.GET['sw']
-    ymin = request.GET['ne']
-    xmax = request.GET['se']
-    ymax = request.GET['nw']
-    bbox = (xmin, ymin, xmax, ymax)
-    geom = Polygon.from_bbox(bbox)
-    lraJson = serialize('geojson',lra.objects.filter(poly__contained=geom), geometry_field='geom', fields=('fulladdress','cost'))
+    lraJson = serialize('geojson',lra.objects.all())
     JsonResponse(lraJson)
 
-def getInfo(request, property_id):
-    import zillow
-    with open("./bin/config/zillow_key.conf", 'r') as f:
+def lra_byId(id):
+    parcelJson = serialize('json',lra.objects.get(pk=id))
+    JsonResponse(parcelJson)
+
+def getInfo(request,property_id):
+    import zillow with open("./bin/config/zillow_key.conf", 'r') as f:
         key = f.readline().replace("\n", "")
-
     api = zillow.ValuationApi()
-
     property = get_object_or_404(Property, pk=property_id)
     selected_property = property.property_list.get(pk=request.POST['address'])
-
     address = selected_property + ', St. Louis, MO'
-
     data = api.GetDeepSearchResults(key, address, postal_code)
-
     return HttpResponse(json.dumps({'foo': 'bar'}), mimetype='application/json')
-
     #return HttpResponseRedirect(reverse('property', args=(property.id,)))
-
-
-
 
 """<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
 <script type="text/javascript">
