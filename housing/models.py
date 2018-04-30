5b7de68b100a33750eb97b707813c7c256339469
Nehemiah I. Dacres <vivacarlie@gmail.com>
2018-04-30 18:19:30 +0000

    fixed screwy models from bad importdb (perhaps from older django version? ) added urls for LRA model request by id, added housing to INSTALLED_APPS in settings fixed requirements file

--- housing/models.py
+++ housing/models.py
@@ -1,28 +1,42 @@
-# This is an auto-generated Django model module created by ogrinspect.
-from django.contrib.gis.geos import Polygon
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from __future__ import unicode_literals
+
 from django.contrib.gis.db import models
-class lra(models.Model):
-    id = models.CharField(max_length=0)
-    record_no = models.IntegerField()
-    handle = models.BigIntegerField()
-    parcelid = models.BigIntegerField()
-    address = models.IntegerField()
-    add_suffix = models.CharField(max_length=0)
-    direction = models.CharField(max_length=0)
-    name = models.CharField(max_length=0)
-    type = models.CharField(max_length=0)
-    class = models.CharField(max_length=0)
-    acquisition_dt = models.CharField(max_length=0)
-    centerx = models.FloatField()
-    centery = models.FloatField()
-    value = models.FloatField()
-    value_estimated = models.IntegerField()
-    value_est_dt = models.CharField(max_length=0)
-    entered_date = models.CharField(max_length=0)
-    usage = models.CharField(max_length=0)
-    cost = models.FloatField()
-    property_source = models.CharField(max_length=0)
-    purchase_type = models.CharField(max_length=0)
-    parcel_status = models.CharField(max_length=0)
-    fulladdress = models.CharField(max_length=0)
-    geom = models.PointField()
+
+
+class Lra(models.Model):
+    ogc_fid = models.AutoField(primary_key=True)
+    id = models.CharField(max_length=-1, blank=True, null=True)
+    record_no = models.IntegerField(blank=True, null=True)
+    handle = models.BigIntegerField(blank=True, null=True)
+    parcelid = models.BigIntegerField(blank=True, null=True)
+    address = models.IntegerField(blank=True, null=True)
+    add_suffix = models.CharField(max_length=-1, blank=True, null=True)
+    direction = models.CharField(max_length=-1, blank=True, null=True)
+    name = models.CharField(max_length=-1, blank=True, null=True)
+    type = models.CharField(max_length=-1, blank=True, null=True)
+    class_field = models.CharField(db_column='class', max_length=-1, blank=True, null=True)  # Field renamed because it was a Python reserved word.
+    acquisition_dt = models.CharField(max_length=-1, blank=True, null=True)
+    centerx = models.FloatField(blank=True, null=True)
+    centery = models.FloatField(blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+    value_estimated = models.IntegerField(blank=True, null=True)
+    value_est_dt = models.CharField(max_length=-1, blank=True, null=True)
+    entered_date = models.CharField(max_length=-1, blank=True, null=True)
+    usage = models.CharField(max_length=-1, blank=True, null=True)
+    cost = models.FloatField(blank=True, null=True)
+    property_source = models.CharField(max_length=-1, blank=True, null=True)
+    purchase_type = models.CharField(max_length=-1, blank=True, null=True)
+    parcel_status = models.CharField(max_length=-1, blank=True, null=True)
+    fulladdress = models.CharField(max_length=-1, blank=True, null=True)
+    wkb_geometry = models.PointField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'lra'
--- housing/urls.py
+++ housing/urls.py
@@ -4,8 +4,7 @@ from housing import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^lra/$', views.lra),
-    url(r'^google/$', views.google_forward ) ,
-    url(r'^zillow/$', include(admin.site.urls)),
+    url(r'^lra/', views.lra),
+    #url(r'^zillow/$', include(admin.site.urls)),
     url('r^lra/byId/(?P<id>d+)/$',views.lra)
 ]
