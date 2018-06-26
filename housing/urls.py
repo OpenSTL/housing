1b0994b515b8894c66bc523ad6ebe1f3126f43ef
Nehemiah I. Dacres <vivacarlie@gmail.com>
2018-06-26 00:16:14 +0000

    added landmarks and publichousing views to urls.py

--- housing/urls.py
+++ housing/urls.py
@@ -4,7 +4,11 @@ from housing import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^landmarks/', views.landmarks),
+    url(r'^landmarks/byId/(?P<id>d+)', views.landmarks_byId),
+    url(r'^publichousing/', views.publichousing),
+    url(r'^publichousing/byId/(?P<id>d+)', views.publichousing_byId),
     url(r'^lra/', views.lra),
-    #url(r'^zillow/$', include(admin.site.urls)),
     url('r^lra/byId/(?P<id>d+)/$',views.lra)
+    #url(r'^zillow/$', include(admin.site.urls)),
 ]
